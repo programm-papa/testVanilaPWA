ee2b3e15d83abe7260707b373b5b5fdd61aea7b9
programm-papa <kpavel_wrk@mail.ru>
2023-08-22 11:44:06 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+async function run() {
+  // A service worker must be registered in order to send notifications on iOS
+  const registration = await navigator.serviceWorker.register(
+    "serviceworker.js",
+    {
+      scope: "./",
+    }
+  );
+
+  const button = document.getElementById("subscribe");
+  button.addEventListener("click", async () => {
+    // Triggers popup to request access to send notifications
+    const result = await window.Notification.requestPermission();
+
+    // If the user rejects the permission result will be "denied"
+    if (result === "granted") {
+      await registration.showNotification("Hello there", {
+        body: "Looking good on iOS!",
+      });
+    }
+  });
+}
+
+run();
